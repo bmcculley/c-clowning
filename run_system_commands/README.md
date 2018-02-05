7468534c51bc09133b3d2fbb0ab6983af61577f8
bmcculley <bmcculley@outlook.com>
2018-02-04 21:34:49 -0500

    adding run system commands

---
+++ run_system_commands/README.md
@@ -0,0 +1,16 @@
+# Run system commands
+
+An example of using popen to run a system command and
+getting the return value
+
+In this case `cat /proc/version` is hard coded.
+
+# Building
+
+ * Clone the project `git clone git@github.com:bmcculley/c-clowning.git`
+ * Navigate into this directory `cd c-clowning/run_system_commands` 
+ * Run `make`
+
+# Using
+
+./run_system_commands
---
+++ run_system_commands/run_system_commands.c
@@ -0,0 +1,30 @@
+/**
+ * An example of using popen to run a system command and 
+ * getting the return value.
+ *
+ * Created by bmcculley
+ * Find this and other c snippets here:
+ * https://github.com/bmcculley/c-clowning
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main( int argc, char** argv)
+{
+    FILE *fp;
+    char rstr[1035];
+    
+    fp = popen("cat /proc/version", "r");
+    if (fp == NULL) {
+        printf("Failed to run command\n");
+        exit(1);
+    }
+    
+    while(fgets(rstr, sizeof(rstr)-1, fp) != NULL) {
+        printf("%s", rstr);
+    }
+    
+    pclose(fp);
+    
+    return 0;
+}
