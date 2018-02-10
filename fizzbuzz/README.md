6ef29deb87bf99d501eff3303dbd7b9b6c1d3fc9
bmcculley <bmcculley@outlook.com>
2018-02-09 23:19:06 -0500

    adding fizzbuzz

---
+++ fizzbuzz/README.md
@@ -0,0 +1,15 @@
+# An example of FizzBuzz
+
+An example of FizzBuzz, when a number is divisible by 3 print Fizz,
+divisible 5 print Buzz, divisible by both print FizzBuzz or else 
+just print the number.
+
+# Building
+
+ * Clone the project `git clone git@github.com:bmcculley/c-clowning.git`
+ * Navigate into this directory `cd c-clowning/fizzbuzz` 
+ * Run `make`
+
+# Using
+
+./example
---
+++ fizzbuzz/example.c
@@ -0,0 +1,31 @@
+/**
+ * An example of FizzBuzz, when a number is divisible by 3 print Fizz,
+ * divisible 5 print Buzz, divisible by both print FizzBuzz or else 
+ * just print the number. 
+ *
+ * Created by bmcculley
+ * Find this and other c snippets here:
+ * https://github.com/bmcculley/c-clowning
+ */
+ #include <stdio.h>
+
+int main()
+{
+
+	for (int i = 0; i < 101; i++) {
+		if (i % 3 == 0 && i % 5 == 0) {
+			printf("FizzBuzz\n");
+		}
+		else if (i % 3 == 0) {
+			printf("Fizz\n");
+		}
+		else if (i % 5 == 0) {
+			printf("Buzz\n");
+		}
+		else {
+			printf("%d\n", i);
+		}
+	}
+	
+	return 0;
+}
